afaaf002274fc800c165f6732603718f5ac212d8
eeelyl <mixasar224@gmail.com>
2024-11-16 20:43:13 +0400

    nice

--- custom/play_prikol.lua
+++ custom/play_prikol.lua
@@ -1,11 +1,13 @@
 local dfpwm = require("cc.audio.dfpwm")
 local speaker = peripheral.find("speaker")
 
-local decoder = dfpwm.make_decoder()
-for chunk in io.lines("/songs/m5o9H95N.wav", 16 * 1024) do
-    local buffer = decoder(chunk)
+while true do
+    local decoder = dfpwm.make_decoder()
+    for chunk in io.lines("/songs/m5o9H95N.wav", 16 * 1024) do
+        local buffer = decoder(chunk)
 
-    while not speaker.playAudio(buffer) do
-        os.pullEvent("speaker_audio_empty")
+        while not speaker.playAudio(buffer) do
+            os.pullEvent("speaker_audio_empty")
+        end
     end
 end
